247fb7f276c38bc5c7df21aa1c8b967f7a2433cd
zekapp <zeki.guler@icloud.com>
2016-02-01 14:34:55 +1100

    init wheel lib

---
+++ app/src/main/java/com/app/progresswheel/MainActivity.java
@@ -0,0 +1,84 @@
+package com.app.progresswheel;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.app.progresviews.ProgressWheel;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressWheel mProgressWheel;
+    private int per = 5;
+    private int step = 20000;
+    private DecimalFormat mformatter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Steps cleared", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                clearSteps();
+            }
+        });
+
+        mProgressWheel = (ProgressWheel) findViewById(R.id.progress);
+
+        mformatter = new DecimalFormat("#,###,###");
+
+        mProgressWheel.setPercentage(per);
+        mProgressWheel.setStepCountText(mformatter.format(step));
+    }
+
+    private void clearSteps() {
+        mProgressWheel.setPercentage(per = 0);
+        mProgressWheel.setStepCountText(mformatter.format(step = 0));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void change(View view) {
+        mProgressWheel.setPercentage(per = per + 72);
+        mProgressWheel.setStepCountText(mformatter.format(step = step + 20000));
+    }
+}
---
+++ progreswheelview/src/main/java/com/app/progresviews/ProgressWheel.java
@@ -0,0 +1,211 @@
+package com.app.progresviews;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.app.progreswheelview.R;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class ProgressWheel extends View {
+
+    //Sizes (with defaults)
+    private float  mBarWidth = 24f;
+    private float mCountTextSize= 24; // TODO: use a default from R.dimen...
+    private float mDefTextSize = 24;
+    private int layoutHeight = 0;
+    private int layoutWidth = 0;
+
+    //Colors (with defaults)
+    private int    mProgressColor   = Color.GREEN;
+    private int    mRimColor        = 0xEEEEEEEE;
+    private int    mCountTextColor  = Color.BLACK;
+    private int    mDefTextColor    = Color.BLACK;
+
+    //Padding (with defaults)
+    private int paddingTop = 5;
+    private int paddingBottom = 5;
+    private int paddingLeft = 5;
+    private int paddingRight = 5;
+
+    //Rectangles
+    private RectF mRimBounds = new RectF();
+    private RectF mProgressBounds = new RectF();
+
+    //Paints
+    private Paint mCirclePaint = new Paint();
+    private Paint mBarPaint = new Paint();
+    private TextPaint mCountTextPaint = new TextPaint();
+    private TextPaint mDefTextPaint = new TextPaint();
+
+
+    private String mCountText = " "; // TODO: use a default from R.string...
+    private String mDefText = " "; // TODO: use a default from R.string...
+
+
+    private float mCountTextWidth;
+    private float mCountTextHeight;
+    private float mDefTextHeight;
+    private float mDefTextWidth;
+
+    // Set percentage
+    private int mPercentage = 0;
+
+    public ProgressWheel(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public ProgressWheel(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs, 0);
+    }
+
+    public ProgressWheel(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(attrs, defStyle);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        layoutWidth = w;
+        layoutHeight = h;
+        setupBounds();
+        setupPaints();
+
+        invalidate();
+    }
+
+
+    private void setupPaints() {
+        mBarPaint.setColor(mProgressColor);
+        mBarPaint.setAntiAlias(true);
+        mBarPaint.setStyle(Paint.Style.STROKE);
+        mBarPaint.setStrokeWidth(mBarWidth);
+        mBarPaint.setStrokeCap(Paint.Cap.ROUND);
+
+
+        mCirclePaint.setColor(mRimColor);
+        mCirclePaint.setAntiAlias(true);
+        mCirclePaint.setStyle(Paint.Style.STROKE);
+        mCirclePaint.setStrokeWidth(mBarWidth);
+
+        mCountTextPaint.setColor(mCountTextColor);
+        mDefTextPaint.setColor(mDefTextColor);
+    }
+
+    private void setupBounds(){
+        int minValue = Math.min(layoutWidth, layoutHeight);
+
+        // Calc the Offset if needed
+        int xOffset = layoutWidth - minValue;
+        int yOffset = layoutHeight - minValue;
+
+        // Offset
+        paddingTop = this.getPaddingTop() + (yOffset / 2);
+        paddingBottom = this.getPaddingBottom() + (yOffset / 2);
+        paddingLeft = this.getPaddingLeft() + (xOffset / 2);
+        paddingRight = this.getPaddingRight() + (xOffset / 2);
+
+        int width = getWidth();
+        int height = getHeight();
+
+        mRimBounds =  new RectF(
+                paddingLeft + mBarWidth,
+                paddingTop + mBarWidth,
+                width - paddingRight - mBarWidth,
+                height - paddingBottom - mBarWidth);
+
+        mProgressBounds = new RectF(
+                paddingLeft + mBarWidth,
+                paddingTop + mBarWidth,
+                width - paddingRight - mBarWidth,
+                height - paddingBottom - mBarWidth);
+
+        // Count number text
+        mCountTextPaint.setTextSize(mCountTextSize);
+        Paint.FontMetrics fontMetrics = mCountTextPaint.getFontMetrics();
+        mCountTextHeight = fontMetrics.bottom;
+        mCountTextWidth = mCountTextPaint.measureText(mCountText);
+
+        // Definition text
+        mDefTextPaint.setTextSize(mDefTextSize);
+        Paint.FontMetrics fontDefMetrics = mDefTextPaint.getFontMetrics();
+        mDefTextHeight = fontDefMetrics.bottom;
+        mDefTextWidth = mDefTextPaint.measureText(mCountText);
+    }
+
+    private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.ProgressWheel, defStyle, 0);
+
+        mCountText      = a.getString(R.styleable.ProgressWheel_countText);
+        mDefText = a.getString(R.styleable.ProgressWheel_definitionText);
+        mBarWidth       = a.getDimension(R.styleable.ProgressWheel_barWidth, mBarWidth);
+        mProgressColor  = a.getColor(R.styleable.ProgressWheel_progressColor, mProgressColor);
+        mRimColor       = a.getColor(R.styleable.ProgressWheel_rimColor, mRimColor);
+        mCountTextColor = a.getColor(R.styleable.ProgressWheel_countTextColor, mCountTextColor);
+        mDefTextColor   = a.getColor(R.styleable.ProgressWheel_defTextColor, mDefTextColor);
+        mCountTextSize  = a.getDimension(R.styleable.ProgressWheel_countTextSize, mCountTextSize);
+        mDefTextSize    = a.getDimension(R.styleable.ProgressWheel_defTextSize, mDefTextSize);
+        mPercentage     = a.getInt(R.styleable.ProgressWheel_percentage, mPercentage);
+
+        a.recycle();
+
+        // Set up a default TextPaint object
+        mCountTextPaint = new TextPaint();
+        mCountTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
+        mCountTextPaint.setTextAlign(Paint.Align.LEFT);
+
+        // Update TextPaint and text measurements from attributes
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawArc(mRimBounds, 0, 360, false, mCirclePaint);
+        canvas.drawArc(mProgressBounds, -90, mPercentage, false, mBarPaint);
+
+        float horizontalCountTextOffset = mCountTextPaint.measureText(mCountText) / 2;
+
+        canvas.drawText(mCountText,
+                    this.getWidth() / 2 - horizontalCountTextOffset,
+                    this.getHeight() / 2 ,
+                    mCountTextPaint
+                );
+
+        float horizontalDefTextOffset = mDefTextPaint.measureText(mDefText) / 2;
+
+        canvas.drawText(mDefText,
+                this.getWidth() / 2 - horizontalDefTextOffset,
+                this.getHeight() / 2 + mCountTextHeight + 20,
+                mDefTextPaint
+        );
+    }
+
+    public void setStepCountText(String countText) {
+        mCountText = countText;
+        invalidate();
+    }
+
+    public void setDefText(String defText) {
+        mDefText = defText;
+        invalidate();
+    }
+
+    public void setPercentage(int per) {
+        mPercentage = per;
+        invalidate();
+    }
+}
